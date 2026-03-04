23768ca2b4a69e3315c297f4c7052f3ecf649d36
Nada Saad <nadasa3d857@gmail.com>
2026-03-05 00:27:08 +0200

    added index page files

---
+++ app.js
@@ -0,0 +1,46 @@
+var app = angular.module('pharmaApp', ['ngRoute']);
+
+app.run(function($rootScope, $location) {
+    $rootScope.isActive = function(viewLocation) {
+        return viewLocation === $location.path();
+    };
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider
+            .when('/', {
+            templateUrl: 'views/dashboard.html',
+            controller: 'dashboardController'
+        })
+        .when('/medicines-list', {
+            templateUrl: 'views/medicines-list.html',
+            controller: 'medicineController'
+        })
+        .when('/invoices-history', {
+            templateUrl: 'views/invoices-history.html',
+            controller: 'posController'
+        })
+        .when('/pos', {
+            templateUrl: 'views/pos.html',
+            controller: 'posController'
+        })
+        .when('/customers-list', {
+            templateUrl: 'views/customers-list.html',
+            controller: 'customerController'
+        })
+        .when('/customers-form', {
+            templateUrl: 'views/customers-form.html',
+            controller: 'customerController'
+        })
+        .when('/medicines-form', {
+            templateUrl: 'views/medicines-form.html',
+            controller: 'medicineController'
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'authController'
+        })
+        .otherwise({
+            redirectTo: '/dashboard'
+        });
+});
